4363bee50dbe3a0169b41b2cc9ce97ca128c08a4
leafaar <yyyyrrzb@gmail.com>
2023-09-28 09:00:47 -0300

    first commit

---
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -0,0 +1,5 @@
+const ServerIdPage = () => {
+  return <div>Server id Pages</div>;
+};
+
+export default ServerIdPage;
---
+++ components/navigation/navigation-sidebar.tsx
@@ -0,0 +1,3 @@
+export const NavigationSidebar = () => {
+  return <div>Navigation Sidebar</div>;
+};
